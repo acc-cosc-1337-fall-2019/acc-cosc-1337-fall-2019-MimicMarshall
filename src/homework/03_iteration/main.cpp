e792fba89c806db4a4fa2328892384149c30a4b2
Benjamin Lubinin <benjamin.lubinin@g.austincc.edu>
2019-09-23 20:32:50 -0500

    HW 3 almost complete with errors

--- src/homework/03_iteration/main.cpp
+++ src/homework/03_iteration/main.cpp
@@ -1,8 +1,11 @@
 //write include statements
-
+#include "dna.h"
+#include<string>
+#include<iostream>
 //write using statements
-
-
+using std::string;
+using std::cout;
+using std::cin;
 /*
 Write code that prompts user to enter 1 for Get GC Content, 
 or 2 for Get DNA Complement.  The program will prompt user for a 
@@ -12,5 +15,26 @@ user enters a y or Y.
 */
 int main() 
 {
-	return 0;
+	string menu_choice = "y";
+	while (menu_choice == "y" || menu_choice == "Y")
+	{
+		string dna = "";
+		cout << "Tell me the DNA sequence that you want to analyze: ";
+		cin >> dna;
+		int menu = 0;
+		cout << "Tell me if you want the (1)GC Content or (2)DNA Complement: ";
+		cin >> menu;
+		switch (menu)
+		{
+		case 1:
+			cout << get_gc_content(dna);
+		case 2:
+			cout << get_dna_complement(dna);
+		default:
+			cout << "Invalid Option\n";
+		}
+		cout << "Do you you want to try again? Press 'Y' to repeat. ";
+		cin >> menu_choice;
+	}
+	
 }
--- test/homework_test/03_iteration/03_iteration_tests.cpp
+++ test/homework_test/03_iteration/03_iteration_tests.cpp
@@ -1,7 +1,23 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "dna.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
+}
+TEST_CASE("Test get_gc_content") 
+{
+	REQUIRE(get_gc_content(AGCTATAG) == .375;
+	REQUIRE(get_gc_content(CGCTATAG) == .50;
+}
+TEST_CASE("Test get_reverse_string")
+{
+	REQUIRE(get_reverse_string(AGCTATAG) == "GATATCGA";
+	REQUIRE(get_reverse_string(CGCTATAG) == "GATATCGC";
+}
+TEST_CASE("Test get_dna_complement")
+{
+	REQUIRE(get_dna_complement(AAAACCCGGT) == "ACCGGGTTTT";
+	REQUIRE(get_dna_complement(CCCGGAAAAT) == "ATTTTCCGGG";
 }
 
