91c35f4210aad89908df19e21778f0d306ce5803
Benjamin Lubinin <benjamin.lubinin@g.austincc.edu>
2019-10-28 18:09:10 -0500

    dereference operator *

--- src/examples/04_module/01_bank/main.cpp
+++ src/examples/04_module/01_bank/main.cpp
@@ -1,20 +1,23 @@
 #include<iostream>
-#include<vector>
-#include<functional>
 #include"checking_account.h"
 #include"savings_account.h"
 
-using std::cout; using std::cin; using std::vector; using std::reference_wrapper;
+using std::cout;
 
 
 int main()
 {
-	CheckingAccount c(1500);
-	SavingsAccount s(500);
-	vector<reference_wrapper<BankAccount>> accounts{c, s};
+	int num = 5;
+	int & num_ref = num; //get the address of num
+	cout << "Address: " << &num_ref << std::endl; //output address
+	cout << "Value at above address: " << num_ref << std::endl; //output value at address of num
+	
+	int *val_ = &num_ref; //update
+	cout << "Value pointed to: " << val_ << std::endl;
+	cout << "Num value: " << *val_ << std::endl;
 
-	for (auto act : accounts)
-		cout << "Balance: " << act.get().get_balance() << "\n";
-
+	*val_ = 15;
+	cout << "Value pointed to: " << val_ << std::endl;
+	cout << "Num value: " << *val_ << std::endl;
 	return 0;
 }
