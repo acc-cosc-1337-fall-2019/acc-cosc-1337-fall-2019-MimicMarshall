be6d08624ff5ba6bcfe82d521e55369c22df8ed6
Benjamin Lubinin <benjamin.lubinin@g.austincc.edu>
2019-10-28 18:24:25 -0500

    dynamic memory

--- enc_temp_folder/ba3ad1c5e219abdaea747d02e2137a7/main.cpp
+++ enc_temp_folder/ba3ad1c5e219abdaea747d02e2137a7/main.cpp
@@ -9,8 +9,10 @@ int main()
 {
 	int * ptr_num = new int(5); //acquire dynamic memory
 	std::cout << "Address of: " << ptr_num << std::endl;
-	std::cout << "Value pointed to: " << *ptr_num;
+	std::cout << "Value pointed to: " << *ptr_num << std::endl;
 
 	delete ptr_num; //YOU MUST REMEMBER TO DELETE ALLOCATED MEMORY
+	std::cout << "Address of: " << ptr_num << std::endl;
+	std::cout << "Value pointed to: " << *ptr_num;
 	return 0;
 }
